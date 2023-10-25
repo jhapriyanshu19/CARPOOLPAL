689e5d6e532be9e8e2b398f5dc27aa7932700011
Priyanshu Kumar <116969540+jhapriyanshu19@users.noreply.github.com>
2023-10-25 23:57:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-// JavaScript code for handling form submissions and search results
 document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('search-form');
     const offerForm = document.getElementById('offer-form');
     const searchResults = document.getElementById('search-results');
+    const confirmRideButton = document.getElementById('confirm-ride');
 
-    // Store user's selection from the "Offer a Ride" form
     let offerSelection = {};
 
     offerForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        // Get user input from the offer form and store it
         const departure = document.getElementById('departure').value;
         const destination = document.getElementById('destination').value;
+        const pickupLocation = document.getElementById('pickup-location').value;
         const departureDate = document.getElementById('departure-date').value;
+        const departureTime = document.getElementById('departure-time').value;
+        const carId = document.getElementById('car-id').value;
         const seats = document.getElementById('seats').value;
+        const contactNumber = document.getElementById('contact-number').value;
 
         offerSelection = {
             departure,
             destination,
+            pickupLocation,
             departureDate,
-            seats
+            departureTime,
+            carId,
+            seats,
+            contactNumber
         };
 
-        // Display a success message or handle errors
         alert('Ride offer created successfully!');
     });
 
     searchForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        // Get user input from the search form and perform a search
         const from = document.getElementById('from').value;
         const to = document.getElementById('to').value;
+        const pickupLocation = document.getElementById('pickup-location').value;
         const date = document.getElementById('date').value;
+        const time = document.getElementById('time').value;
 
-        // Clear previous search results
         searchResults.innerHTML = '';
 
-        // Display user's selection from the "Offer a Ride" form in search results
         const resultItem = document.createElement('div');
         resultItem.className = 'result-item';
         resultItem.innerHTML = `
-            <h3>Ride from ${offerSelection.departure} to ${offerSelection.destination}</h3>
-            <p>Departure Date: ${offerSelection.departureDate}</p>
+            <h3>Ride from ${from} to ${to}</h3>
+            <p>Pickup Location: ${pickupLocation}</p>
+            <p>Departure Date: ${date}</p>
+            <p>Departure Time: ${time}</p>
+            <p>Car ID: ${offerSelection.carId}</p>
             <p>Available Seats: ${offerSelection.seats}</p>
+            <p>Contact Number: ${offerSelection.contactNumber}</p>
         `;
+
         searchResults.appendChild(resultItem);
+        confirmRideButton.style.display = 'block';
     });
 });
